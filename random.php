9a0abebb926f69682f4401c44d4c15d3b13f934e
Sifaurrahman <sifaurrahman11111@gmail.com>
2025-05-20 14:32:36 +0300

    Initial commit: add random.php demo

---
+++ random.php
@@ -0,0 +1,9 @@
+<?php
+// random.php
+// A simple script that prints a random number between 1 and 100
+
+// Generate a random number
+$number = rand(1, 100);
+
+// Output to the browser/console
+echo "Your random number is: " . $number . "\n";
